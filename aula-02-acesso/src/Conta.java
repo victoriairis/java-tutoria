4625b48d3e8d95c83f1d40683a0497aa3f305d0b
Yahto Code <yahtocode@gmail.com>
2020-03-16 21:00:22 -0300

    criacao da aula sobre modificadores de acesso

---
+++ aula-02-acesso/src/Conta.java
@@ -0,0 +1,16 @@
+/** 
+ * @author irisroques
+ * 
+ * */
+public class Conta {
+	
+	private double limite, saldo;
+	private String titular;
+	
+	/*
+	 * As palavras reservadas private, protected e public são aplicadas para 
+	 * controlar acesso aos membros das classes, ou seja, fazer com que haja mais
+	 * segurança 
+	 *  */
+	
+}
---
+++ aula-02-acesso/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author irisroques
+ * 
+ * Sobre modificadores de acesso, getters e setters, this e construtores.
+ *  */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
